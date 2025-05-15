f10e251d33c38b7b2351bd787f353c22888bd61c
cuiziwei <cuiziwei@xiaomi.com>
2025-05-15 20:22:38 +0800

    libcxx: Prevent errors caused by inconsistent streamoff types after basic_stringbuf template specialization.
    Signed-off-by: cuiziwei <cuiziwei@xiaomi.com>

--- include/__fwd/ios.h
+++ include/__fwd/ios.h
@@ -29,12 +29,7 @@ using wios = basic_ios<wchar_t>;
 template <class _CharT, class _Traits>
 class _LIBCPP_PREFERRED_NAME(ios) _LIBCPP_IF_WIDE_CHARACTERS(_LIBCPP_PREFERRED_NAME(wios)) basic_ios;
 
-#if defined(_NEWLIB_VERSION)
-// On newlib, off_t is 'long int'
-using streamoff = long int; // for char_traits in <string>
-#else
-using streamoff = long long; // for char_traits in <string>
-#endif
+using streamoff = long long;
 
 _LIBCPP_END_NAMESPACE_STD
 
